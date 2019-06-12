5413281f2640531603b4eab8b918acf3104abdb2
Sara Ong <saralijinnong@dyn-118-138-22-33.its.monash.edu.au>
2019-06-12 21:16:49 +1000

    -Created initial workspace

---
+++ FIT3178 Final App/FIT3178 Final App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT3178 Final App
+//
+//  Created by Sara Ong  on 12/06/2019.
+//  Copyright © 2019 Laoqi He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FIT3178 Final App/FIT3178 Final AppTests/FIT3178_Final_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  FIT3178_Final_AppTests.swift
+//  FIT3178 Final AppTests
+//
+//  Created by Sara Ong  on 12/06/2019.
+//  Copyright © 2019 Laoqi He. All rights reserved.
+//
+
+import XCTest
+@testable import FIT3178_Final_App
+
+class FIT3178_Final_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
