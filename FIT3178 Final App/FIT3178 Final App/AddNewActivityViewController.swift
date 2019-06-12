7a56371e6ee0e3a3c677d297588f73c21633a775
Sara Ong <saralijinnong@dyn-118-138-22-33.its.monash.edu.au>
2019-06-13 00:27:52 +1000

    Added base views

---
+++ FIT3178 Final App/FIT3178 Final App/AddNewActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewActivityViewController.swift
+//  FIT3178 Final App
+//
+//  Created by Sara Ong  on 13/06/2019.
+//  Copyright © 2019 Laoqi He. All rights reserved.
+//
+
+import UIKit
+
+class AddNewActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FIT3178 Final App/FIT3178 Final App/NewItineraryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewItineraryViewController.swift
+//  FIT3178 Final App
+//
+//  Created by Sara Ong  on 12/06/2019.
+//  Copyright © 2019 Laoqi He. All rights reserved.
+//
+
+import UIKit
+
+class NewItineraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
